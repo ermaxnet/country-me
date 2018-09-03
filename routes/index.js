e16d1e175872ebd4f240317ca79492b4e848bd60
Maksim Yaromin <eremin_my@outlook.com>
2018-09-03 21:40:04 +0300

    Fix #2

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ const { Country } = require("../database");
 
 module.exports = function(router) {
     router.get("/", (req, res, next) => {
-        const ip = req.headers["X-Forwarded-For"]
-            || req.headers["X-Forwarded-For"]
-            || req.client.remoteAddress;
-        getCountryCode(ip).then(({ country_code }) => {
+        getCountryCode(req.ip).then(({ country_code }) => {
             return Country.findOne({ $or: [
                 { "alpha2Code": country_code },
                 { "alpha3Code": country_code }
