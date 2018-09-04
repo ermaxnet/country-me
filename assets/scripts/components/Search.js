1bacae980a74b0770fbed51f4393065117e2961e
Maksim Yaromin <eremin_my@outlook.com>
2018-09-04 16:57:58 +0300

    Added Search Component

--- assets/scripts/components/Search.js
+++ assets/scripts/components/Search.js
@@ -1,3 +1,41 @@
-import React from "react";
+import React, { Component as ReactComponent } from "react";
 import ReactDOM from "react-dom";
+import locale from "../../../locales";
 import "components/Search.scss";
+
+class SearchComponent extends ReactComponent {
+    state = {
+        match: ""
+    }
+
+    onChange(e) {
+        this.setState({ match: e.target.value });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <form
+                className="Search__form"
+                action="/search"
+                onSubmit={this.onSubmit.bind(this)}
+            >
+                <input 
+                    type="text"
+                    name="match"
+                    value={this.state.match}
+                    onChange={this.onChange.bind(this)}
+                    placeholder={locale.t("search_placeholder")}
+                    className="Search__input"
+                />
+                <span className="icon icon-search"></span>
+            </form>
+        );
+    }
+};
+
+ReactDOM.render(
+    <SearchComponent />,
+    document.getElementById("search")
+);
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -94,6 +94,7 @@ module.exports = {
                         loader: "babel-loader",
                         options: {
                             presets: [ "@babel/preset-env", "@babel/preset-react" ],
+                            plugins: [ "@babel/plugin-proposal-class-properties" ],
                             cacheDirectory: true
                         }
                     },
